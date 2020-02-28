e5fb3128eeceb5738eee4d137f4e18173acbdaa0
secalquin <se.calquin@alumnos.duoc.cl>
2020-02-27 22:57:51 -0300

    LOWER

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,15 @@
-const mysqlConnection = require('../database'); //DatabaseConexion
-const { validationResult } = require('express-validator'); //Library for validate request.
+const mysqlconnection = require('../database'); //databaseconexion
+const { validationresult } = require('express-validator'); //library for validate request.
 const controller = {};
 
 controller.list = (req, res) => {
-let getProducts = "SELECT PRD.upc as prd_upc,PRD.long_description as prd_longdesc,PRD.short_description as prd_shortdesc,PRD.price as prd_price,PRD.iva as prd_iva,PRD.sku as prd_sku,PRD.name as prd_name,PRD.id as prd_id,WAR.id as war_id,WAR.name as war_name,WAR.stock as war_stockb FROM PRODUCTS PRD LEFT JOIN WAREHOUSE WAR ON WAR.PRODUCTS_ID = PRD.ID";
-	mysqlConnection.query(getProducts,(err,rowsPrd,fields) =>{
+let getproducts = "select prd.upc as prd_upc,prd.long_description as prd_longdesc,prd.short_description as prd_shortdesc,prd.price as prd_price,prd.iva as prd_iva,prd.sku as prd_sku,prd.name as prd_name,prd.id as prd_id,war.id as war_id,war.name as war_name,war.stock as war_stockb from products prd left join warehouse war on war.products_id = prd.id";
+	mysqlconnection.query(getproducts,(err,rowsprd,fields) =>{
 		var result = [], index = {};
 		if(!err){
-			//res.json(rowsPrd);
+			//res.json(rowsprd);
 			
-			rowsPrd.forEach(function (row) {
+			rowsprd.foreach(function (row) {
 		        if ( !(row.prd_id in index) ) {
 		            index[row.prd_id] = {
 		                id: row.prd_id,
@@ -41,18 +41,18 @@ let getProducts = "SELECT PRD.upc as prd_upc,PRD.long_description as prd_longdes
 
 controller.find = (req,res) => {
 const { id } = req.params;
-const errors = validationResult(req);
-	if (!errors.isEmpty()) {
+const errors = validationresult(req);
+	if (!errors.isempty()) {
 	  return res.status(422).json({ errors: errors.array() });
 	}
 	
-let getProduct = "SELECT PRD.upc as prd_upc,PRD.long_description as prd_longdesc,PRD.short_description as prd_shortdesc,PRD.price as prd_price,PRD.iva as prd_iva,PRD.sku as prd_sku,PRD.name as prd_name,PRD.id as prd_id,WAR.id as war_id,WAR.name as war_name,WAR.stock as war_stock FROM PRODUCTS PRD LEFT JOIN WAREHOUSE WAR ON WAR.PRODUCTS_ID = PRD.ID WHERE PRD.ID = ?";
-	mysqlConnection.query(getProduct,[id],(err,rowsPrd,fields) =>{
+let getproduct = "select prd.upc as prd_upc,prd.long_description as prd_longdesc,prd.short_description as prd_shortdesc,prd.price as prd_price,prd.iva as prd_iva,prd.sku as prd_sku,prd.name as prd_name,prd.id as prd_id,war.id as war_id,war.name as war_name,war.stock as war_stock from products prd left join warehouse war on war.products_id = prd.id where prd.id = ?";
+	mysqlconnection.query(getproduct,[id],(err,rowsprd,fields) =>{
 		var result = [], index = {};
 		if(!err){
-			//res.json(rowsPrd);
+			//res.json(rowsprd);
 
-			rowsPrd.forEach(function (row) {
+			rowsprd.foreach(function (row) {
 		        if ( !(row.prd_id in index) ) {
 		            index[row.prd_id] = {
 		                id: row.prd_id,
@@ -82,16 +82,16 @@ let getProduct = "SELECT PRD.upc as prd_upc,PRD.long_description as prd_longdesc
 
 controller.save = (req,res) =>{
 	const { name, sku, upc, long_description, short_description, price } = req.body;
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
+	const errors = validationresult(req);
+	if (!errors.isempty()) {
 	  return res.status(422).json({ errors: errors.array() });
 	}
 
-	//Validate Request.
-	mysqlConnection.query("CALL new_product(?,?,?,?,?,?)",[name, sku, upc, long_description, short_description, price],(err,rows,fields) =>{
+	//validate request.
+	mysqlconnection.query("call new_product(?,?,?,?,?,?)",[name, sku, upc, long_description, short_description, price],(err,rows,fields) =>{
 
 		if(!err){
-			return res.status(201).json({message: "Product was created correctly",status: "201"});
+			return res.status(201).json({message: "product was created correctly",status: "201"});
 		}else{
 			console.log(err);
 		}
@@ -101,8 +101,8 @@ controller.save = (req,res) =>{
 controller.update = (req, res) => {
 
 	const { name, sku, upc, long_description, short_description, price } = req.body;
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
+	const errors = validationresult(req);
+	if (!errors.isempty()) {
 	  return res.status(422).json({ errors: errors.array() });
 	}
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,154 +1,154 @@
 const express = require('express');
-const mysqlConnection = require('../database'); //DatabaseConexion
-const router = express.Router(); //Router, create route for access
-const { check, oneOf } = require('express-validator'); //Library for validate request.
+const mysqlconnection = require('../database'); //databaseconexion
+const router = express.router(); //router, create route for access
+const { check, oneof } = require('express-validator'); //library for validate request.
 
-const productController = require('../controllers/productController');
+const productcontroller = require('../controllers/productcontroller');
 
 
 
-router.get('/products', productController.list);
-router.get('/products/:id',[
-oneOf([
-        check('id')
-		.custom(value => {
-				return new Promise((resolve, reject) => {
-					//reject(new Error('id Already exists ' + value));
-					let validateExitsID = "SELECT id FROM PRODUCTS WHERE id LIKE ?";
-					mysqlConnection.query(validateExitsID,[value],(err,results,fields) =>{
-						if (err){
+router.get('/products', productcontroller.list);
+router.get('/products/:id', [
+	oneof([
+		check('id')
+			.custom(value => {
+				return new promise((resolve, reject) => {
+					//reject(new error('id already exists ' + value));
+					let validateexitsid = "select id from products where id like ?";
+					mysqlconnection.query(validateexitsid, [value], (err, results, fields) => {
+						if (err) {
 							reject(err);
 						}
-			             if (results.length>0){
-			             	resolve();
-			             }else{
-			             	reject(new Error('ID does not exist'));
-			             }
-					})
-			    })
-			})	
-    ])
-],productController.find);
-
-router.post('/product/:id',[
-	oneOf([
-        check('id')
-		.custom(value => {
-				return new Promise((resolve, reject) => {
-					//reject(new Error('id Already exists ' + value));
-					let validateExitsID = "SELECT id FROM PRODUCTS WHERE id LIKE ?";
-					mysqlConnection.query(validateExitsID,[value],(err,results,fields) =>{
-						if (err){
-							reject(err);
+						if (results.length > 0) {
+							resolve();
+						} else {
+							reject(new error('id does not exist'));
 						}
-			             if (results.length>0){
-			             	resolve();
-			             }else{
-			             	reject(new Error('ID does not exist'));
-			             }
 					})
-			    })
-			})	
-    ])
-
-],productController.update);
-
-
-
-
-
-router.post('/product',[
-	/*
-	 * Validate Request.
-     */
-	oneOf([
-        check('name')
-		.exists().withMessage('name is required')
-		.trim().not().isEmpty().withMessage('name cannot be empty')
-		.isLength({ min: 10 }).withMessage('must be at least 10 chars long')
-		.isLength({ max: 100 }).withMessage('must be at maximum of 100 characters long'),
-    ]),
-
-	oneOf([
-	       check('sku')
-		  .not()
-		  .isEmpty()
-		  .trim() //trims characters (whitespace by default) at the beginning and at the end of a string
-		  .escape() //replaces <, >, &, ', " and / with their corresponding HTML entities
-		  .isLength({ min: 5 }).withMessage('must be at least 5 chars long')
-		  .isLength({ max: 15 }).withMessage('must be at maximum of 15 characters long')
-		  .custom(value => {
-				return new Promise((resolve, reject) => {
-					let validateBySKU = "SELECT sku FROM PRODUCTS WHERE SKU LIKE ?";
-					mysqlConnection.query(validateBySKU,[value],(err,results,fields) =>{
-						if (err){
-							reject(err);
-						}
-			             if (results.length>0){
-			             	reject(new Error('SKU Already exists'));
-			             }else{
-			             	resolve();
-			             }
-					})
-			    })
+				})
 			})
-	]),
+	])
+], productcontroller.find);
 
-	oneOf([
-	       check('upc')
-		  .not()
-		  .isEmpty()
-		  .trim() //trims characters (whitespace by default) at the beginning and at the end of a string
-		  .escape() //replaces <, >, &, ', " and / with their corresponding HTML entities
-		  .isLength({ min: 8 }).withMessage('must be at least 8 chars long')
-		  .isLength({ max: 11 }).withMessage('must be at maximum of 15 characters long')
-		  .custom(value => {
-				return new Promise((resolve, reject) => {
-					let validateByUPC = "SELECT upc FROM PRODUCTS WHERE UPC LIKE ?";
-					mysqlConnection.query(validateByUPC,[value],(err,results,fields) =>{
-						if (err){
+router.post('/product/:id', [
+	oneof([
+		check('id')
+			.custom(value => {
+				return new promise((resolve, reject) => {
+					//reject(new error('id already exists ' + value));
+					let validateexitsid = "select id from products where id like ?";
+					mysqlconnection.query(validateexitsid, [value], (err, results, fields) => {
+						if (err) {
 							reject(err);
 						}
-			             if (results.length>0){
-			             	reject(new Error('UPC Already exists'));
-			             }else{
-			             	resolve();
-			             }
+						if (results.length > 0) {
+							resolve();
+						} else {
+							reject(new error('id does not exist'));
+						}
 					})
-			    })
+				})
 			})
-	]),
-
-	oneOf([
-	       check('long_description')
-		  .not()
-		  .isEmpty()
-		  .trim() //trims characters (whitespace by default) at the beginning and at the end of a string
-		  .escape() //replaces <, >, &, ', " and / with their corresponding HTML entities
-		  .isLength({ min: 1 }).withMessage('must be at least 1 chars long')
-		  .isLength({ max: 10000 }).withMessage('must be at maximum of 10000 characters long')
-	]),
-
-	oneOf([
-	       check('short_description')
-		  .not()
-		  .isEmpty()
-		  .trim() //trims characters (whitespace by default) at the beginning and at the end of a string
-		  .escape() //replaces <, >, &, ', " and / with their corresponding HTML entities
-		  .isLength({ min: 1 }).withMessage('must be at least 1 chars long')
-		  .isLength({ max: 1000 }).withMessage('must be at maximum of 1000 characters long')
-	]),
-
-	oneOf([
-	       check('price')
-		  .not()
-		  .isEmpty()
-		  .isNumeric().withMessage('must be at number')
-		  .isLength({ min: 1 }).withMessage('must be at least 8 chars long')
-		  .isLength({ max: 20 }).withMessage('must be at maximum of 15 characters long')
 	])
 
-],productController.save);
+], productcontroller.update);
+
+
+
+
+
+router.post('/product', [
+	/*
+	 * validate request.
+     */
+	oneof([
+		check('name')
+			.exists().withmessage('name is required')
+			.trim().not().isempty().withmessage('name cannot be empty')
+			.islength({ min: 10 }).withmessage('must be at least 10 chars long')
+			.islength({ max: 100 }).withmessage('must be at maximum of 100 characters long'),
+	]),
+
+	oneof([
+		check('sku')
+			.not()
+			.isempty()
+			.trim() //trims characters (whitespace by default) at the beginning and at the end of a string
+			.escape() //replaces <, >, &, ', " and / with their corresponding html entities
+			.islength({ min: 5 }).withmessage('must be at least 5 chars long')
+			.islength({ max: 15 }).withmessage('must be at maximum of 15 characters long')
+			.custom(value => {
+				return new promise((resolve, reject) => {
+					let validatebysku = "select sku from products where sku like ?";
+					mysqlconnection.query(validatebysku, [value], (err, results, fields) => {
+						if (err) {
+							reject(err);
+						}
+						if (results.length > 0) {
+							reject(new error('sku already exists'));
+						} else {
+							resolve();
+						}
+					})
+				})
+			})
+	]),
+
+	oneof([
+		check('upc')
+			.not()
+			.isempty()
+			.trim() //trims characters (whitespace by default) at the beginning and at the end of a string
+			.escape() //replaces <, >, &, ', " and / with their corresponding html entities
+			.islength({ min: 8 }).withmessage('must be at least 8 chars long')
+			.islength({ max: 11 }).withmessage('must be at maximum of 15 characters long')
+			.custom(value => {
+				return new promise((resolve, reject) => {
+					let validatebyupc = "select upc from products where upc like ?";
+					mysqlconnection.query(validatebyupc, [value], (err, results, fields) => {
+						if (err) {
+							reject(err);
+						}
+						if (results.length > 0) {
+							reject(new error('upc already exists'));
+						} else {
+							resolve();
+						}
+					})
+				})
+			})
+	]),
+
+	oneof([
+		check('long_description')
+			.not()
+			.isempty()
+			.trim() //trims characters (whitespace by default) at the beginning and at the end of a string
+			.escape() //replaces <, >, &, ', " and / with their corresponding html entities
+			.islength({ min: 1 }).withmessage('must be at least 1 chars long')
+			.islength({ max: 10000 }).withmessage('must be at maximum of 10000 characters long')
+	]),
+
+	oneof([
+		check('short_description')
+			.not()
+			.isempty()
+			.trim() //trims characters (whitespace by default) at the beginning and at the end of a string
+			.escape() //replaces <, >, &, ', " and / with their corresponding html entities
+			.islength({ min: 1 }).withmessage('must be at least 1 chars long')
+			.islength({ max: 1000 }).withmessage('must be at maximum of 1000 characters long')
+	]),
+
+	oneof([
+		check('price')
+			.not()
+			.isempty()
+			.isnumeric().withmessage('must be at number')
+			.islength({ min: 1 }).withmessage('must be at least 8 chars long')
+			.islength({ max: 20 }).withmessage('must be at maximum of 15 characters long')
+	])
+
+], productcontroller.save);
 
 
 module.exports = router;
